dbbad0219d91e442b837d719c5fc345722abeafd
Andrey Freitas <yanusckiewicz@profusion.mobi>
2023-05-26 14:18:16 -0300

    fix(columnwrap): remove column wrapping component

--- app/src/main/java/com/example/androidenhancedvideoplayer/MainActivity.kt
+++ app/src/main/java/com/example/androidenhancedvideoplayer/MainActivity.kt
@@ -3,26 +3,17 @@ package com.example.androidenhancedvideoplayer
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import com.example.androidenhancedvideoplayer.components.VideoPlayerComponent
 import com.example.androidenhancedvideoplayer.ui.theme.AndroidEnhancedVideoPlayerTheme
+import com.profusion.androidenhancedvideoplayer.components.EnhancedVideoPlayer
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             AndroidEnhancedVideoPlayerTheme {
-                Column(
-                    Modifier.fillMaxSize(),
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                    verticalArrangement = Arrangement.Center
-                ) {
-                    VideoPlayerComponent(resourceId = R.raw.login_screen_background)
-                }
+                EnhancedVideoPlayer(
+                    resourceId = R.raw.login_screen_background
+                )
             }
         }
     }
