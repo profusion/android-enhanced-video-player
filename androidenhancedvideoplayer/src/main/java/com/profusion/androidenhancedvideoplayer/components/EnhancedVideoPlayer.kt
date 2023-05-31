74b0fc34ae106c9a7eda9585659c235b9450b183
ricardodalarme <ricardo.dalarme@profusion.mobi>
2023-05-31 14:31:55 -0300

    feat: add basic controls
    - Implement a basic play, pause, previous and next buttons the Jepack
      Compose
    - Also allow who ever is using the library to provide their own
      controls
    
    Closes #14

--- androidenhancedvideoplayer/src/main/java/com/profusion/androidenhancedvideoplayer/components/EnhancedVideoPlayer.kt
+++ androidenhancedvideoplayer/src/main/java/com/profusion/androidenhancedvideoplayer/components/EnhancedVideoPlayer.kt
@@ -1,8 +1,16 @@
 package com.profusion.androidenhancedvideoplayer.components
 
 import android.net.Uri
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Box
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.media3.common.MediaItem
@@ -14,15 +22,14 @@ import androidx.media3.ui.PlayerView
 
 private const val MAIN_PACKAGE_PATH_PREFIX = "android.resource://"
 
-@androidx.annotation.OptIn(UnstableApi::class)
 @Composable
 fun EnhancedVideoPlayer(
     resourceId: Int,
-    useControls: Boolean = false,
     alwaysRepeat: Boolean = true,
     fullScreen: Boolean = true,
     playImmediately: Boolean = true,
-    soundOff: Boolean = true
+    soundOff: Boolean = true,
+    controlsCustomization: ControlsCustomization = ControlsCustomization()
 ) {
     val context = LocalContext.current
     val mainPackagePath = "$MAIN_PACKAGE_PATH_PREFIX${context.packageName}/"
@@ -41,17 +48,68 @@ fun EnhancedVideoPlayer(
             }
     }
 
-    AndroidView(
-        factory = { factoryContext ->
-            PlayerView(factoryContext).apply {
-                player = exoPlayer
-                useController = useControls
-                resizeMode = if (fullScreen) {
-                    AspectRatioFrameLayout.RESIZE_MODE_ZOOM
-                } else {
-                    AspectRatioFrameLayout.RESIZE_MODE_FIT
-                }
+    var isPlaying by remember { mutableStateOf(exoPlayer.isPlaying) }
+    var isControlsVisible by remember { mutableStateOf(false) }
+
+    DisposableEffect(context) {
+        val listener = object : Player.Listener {
+            override fun onIsPlayingChanged(value: Boolean) {
+                isPlaying = value
+                super.onIsPlayingChanged(value)
             }
         }
+        exoPlayer.addListener(listener)
+
+        onDispose {
+            exoPlayer.removeListener(listener)
+        }
+    }
+
+    VideoPlayer(
+        exoPlayer = exoPlayer,
+        fullScreen = fullScreen,
+        onPlayerClick = { isControlsVisible = !isControlsVisible }
     )
+
+    PlayerControls(
+        isVisible = isControlsVisible,
+        isPlaying = isPlaying,
+        onPreviousClick = exoPlayer::seekToPrevious,
+        onNextClick = exoPlayer::seekToNext,
+        onPauseToggle = when (isPlaying) {
+            true -> exoPlayer::pause
+            false -> exoPlayer::play
+        },
+        customization = controlsCustomization
+    )
+}
+
+@androidx.annotation.OptIn(UnstableApi::class)
+@Composable
+private fun VideoPlayer(
+    exoPlayer: ExoPlayer,
+    fullScreen: Boolean,
+    onPlayerClick: () -> Unit
+) {
+    Box(
+        modifier = Modifier.clickable(
+            indication = null,
+            interactionSource = remember { MutableInteractionSource() },
+            onClick = onPlayerClick
+        )
+    ) {
+        AndroidView(
+            factory = { factoryContext ->
+                PlayerView(factoryContext).apply {
+                    player = exoPlayer
+                    useController = false
+                    resizeMode = if (fullScreen) {
+                        AspectRatioFrameLayout.RESIZE_MODE_ZOOM
+                    } else {
+                        AspectRatioFrameLayout.RESIZE_MODE_FIT
+                    }
+                }
+            }
+        )
+    }
 }
---
+++ androidenhancedvideoplayer/src/main/java/com/profusion/androidenhancedvideoplayer/components/PlayerControls.kt
@@ -0,0 +1,95 @@
+package com.profusion.androidenhancedvideoplayer.components
+
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.IconButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import com.profusion.androidenhancedvideoplayer.R
+
+class ControlsCustomization(
+    val previousIconContent: @Composable () -> Unit = { DefaultPreviousIcon() },
+    val playIconContent: @Composable () -> Unit = { DefaultPlayIcon() },
+    val pauseIconContent: @Composable () -> Unit = { DefaultPauseIcon() },
+    val nextIconContent: @Composable () -> Unit = { DefaultNextIcon() },
+    val modifier: Modifier = Modifier
+)
+
+@Composable
+fun PlayerControls(
+    isVisible: Boolean,
+    isPlaying: Boolean,
+    onPreviousClick: () -> Unit,
+    onPauseToggle: () -> Unit,
+    onNextClick: () -> Unit,
+    customization: ControlsCustomization
+) {
+    AnimatedVisibility(
+        visible = isVisible,
+        enter = fadeIn(),
+        exit = fadeOut()
+    ) {
+        Row(
+            horizontalArrangement = Arrangement.SpaceEvenly,
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = customization.modifier
+                .background(Color.Black.copy(alpha = 0.6f))
+                .fillMaxSize()
+        ) {
+            IconButton(onClick = onPreviousClick) {
+                customization.previousIconContent()
+            }
+            IconButton(onClick = onPauseToggle) {
+                when (isPlaying) {
+                    true -> customization.pauseIconContent()
+                    false -> customization.playIconContent()
+                }
+            }
+            IconButton(onClick = onNextClick) {
+                customization.nextIconContent()
+            }
+        }
+    }
+}
+
+@Composable
+private fun DefaultPreviousIcon() {
+    Image(
+        painter = painterResource(id = R.drawable.ic_skip_previous),
+        contentDescription = stringResource(id = R.string.controls_previous_description)
+    )
+}
+
+@Composable
+private fun DefaultPlayIcon() {
+    Image(
+        painter = painterResource(id = R.drawable.ic_play),
+        contentDescription = stringResource(id = R.string.controls_play_description)
+    )
+}
+
+@Composable
+private fun DefaultPauseIcon() {
+    Image(
+        painter = painterResource(id = R.drawable.ic_pause),
+        contentDescription = stringResource(id = R.string.controls_pause_description)
+    )
+}
+
+@Composable
+private fun DefaultNextIcon() {
+    Image(
+        painter = painterResource(id = R.drawable.ic_skip_next),
+        contentDescription = stringResource(R.string.controls_next_description)
+    )
+}
