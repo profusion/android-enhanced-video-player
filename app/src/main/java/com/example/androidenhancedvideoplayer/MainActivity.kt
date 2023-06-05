4d0203e302aafce3d4c9379bbb827eb38a95ce0d
Andrey Freitas <yanusckiewicz@profusion.mobi>
2023-06-05 09:23:15 -0300

    feat(ThumbList): add RecommendedVideoComponent to example app
    - add a component for mocking video recomendations
    - add a list of this component to example app
    
    closes #34

--- app/src/main/java/com/example/androidenhancedvideoplayer/MainActivity.kt
+++ app/src/main/java/com/example/androidenhancedvideoplayer/MainActivity.kt
@@ -3,7 +3,14 @@ package com.example.androidenhancedvideoplayer
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalConfiguration
+import com.example.androidenhancedvideoplayer.components.RecommendedVideosComponent
 import com.example.androidenhancedvideoplayer.ui.theme.AndroidEnhancedVideoPlayerTheme
+import com.example.androidenhancedvideoplayer.utils.fillMaxSizeOnLandscape
 import com.profusion.androidenhancedvideoplayer.components.EnhancedVideoPlayer
 import com.profusion.androidenhancedvideoplayer.components.playerOverlay.SettingsControlsCustomization
 
@@ -12,13 +19,26 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             AndroidEnhancedVideoPlayerTheme {
-                EnhancedVideoPlayer(
-                    resourceId = R.raw.login_screen_background,
-                    alwaysRepeat = false,
-                    settingsControlsCustomization = SettingsControlsCustomization(
-                        speeds = listOf(0.5f, 1f, 2f, 4f)
-                    )
-                )
+                val orientation = LocalConfiguration.current.orientation
+                Column(
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    Box(
+                        modifier = Modifier
+                            .fillMaxSizeOnLandscape(orientation = orientation)
+                            .fillMaxWidth()
+                    ) {
+                        EnhancedVideoPlayer(
+                            resourceId = R.raw.login_screen_background,
+                            expandContent = true,
+                            alwaysRepeat = false,
+                            settingsControlsCustomization = SettingsControlsCustomization(
+                                speeds = listOf(0.5f, 1f, 2f, 4f)
+                            )
+                        )
+                    }
+                    RecommendedVideosComponent()
+                }
             }
         }
     }
