13e23e5218411405e0a89d0467b1bf658a8190e2
ricardodalarme <ricardo.dalarme@profusion.mobi>
2023-06-07 10:05:21 -0300

    fix(tests): adjust wrong video player constructor

--- androidenhancedvideoplayer/src/androidTest/java/com/profusion/androidenhancedvideoplayer/test/SettingsTest.kt
+++ androidenhancedvideoplayer/src/androidTest/java/com/profusion/androidenhancedvideoplayer/test/SettingsTest.kt
@@ -1,10 +1,10 @@
 package com.profusion.androidenhancedvideoplayer.test
 
+import DefaultPlayerControls
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.performClick
-import defaultPlayerControls
 import org.junit.Rule
 import org.junit.Test
 
@@ -15,7 +15,7 @@ class SettingsTest {
     @Test
     fun settings_WhenClickingOnSpeedButtonShouldShowSpeedSelector() {
         composeTestRule.setContent {
-            defaultPlayerControls()
+            DefaultPlayerControls()
         }
 
         composeTestRule.onNodeWithTag("Playback SpeedSettingsSelector").assertDoesNotExist()
