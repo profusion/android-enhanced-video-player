04fe571bc353012bc52f7f168da5a34568f0db60
ricardodalarme <ricardo.dalarme@profusion.mobi>
2023-06-07 09:02:38 -0300

    feat: display the current time in the bottom bar
    - Add a Text component to the bottom bar to display the current time and
      duration of the video
    - Add a DateUtils class to format the time
    
    Closes #26

---
+++ androidenhancedvideoplayer/src/main/java/com/profusion/androidenhancedvideoplayer/utils/DateUtils.kt
@@ -0,0 +1,27 @@
+package com.profusion.androidenhancedvideoplayer.utils
+
+const val MILLISECONDS_PER_SECOND = 1000
+const val SECONDS_PER_MINUTE = 60
+
+fun formatElapsedTime(value: Long): String {
+    val seconds = value.millisecondsToSeconds % SECONDS_PER_MINUTE
+    val minutes = value.millisecondsToMinutes % SECONDS_PER_MINUTE
+    val hours = value.millisecondsToHours
+
+    return if (hours > 0) {
+        "%02d:%02d:%02d".format(hours, minutes, seconds)
+    } else {
+        "%02d:%02d".format(minutes, seconds)
+    }
+}
+
+fun formatElapsedTime(current: Long, total: Long): String {
+    return "${formatElapsedTime(current)} / ${formatElapsedTime(total)}"
+}
+
+val Long.millisecondsToSeconds: Long
+    get() = this / MILLISECONDS_PER_SECOND
+val Long.millisecondsToMinutes: Long
+    get() = this / (MILLISECONDS_PER_SECOND * SECONDS_PER_MINUTE)
+val Long.millisecondsToHours: Long
+    get() = this / (MILLISECONDS_PER_SECOND * SECONDS_PER_MINUTE * SECONDS_PER_MINUTE)
