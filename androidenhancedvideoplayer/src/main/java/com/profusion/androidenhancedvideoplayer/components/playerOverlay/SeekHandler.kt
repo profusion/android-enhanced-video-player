fd10066e7679508e9ab1765714cdd352523410ea
Thalys Matias Carrara <thalys.carrara@profusion.mobi>
2023-06-07 21:38:06 -0400

    feat: create SeekHandler component
    Closes #15

---
+++ androidenhancedvideoplayer/src/main/java/com/profusion/androidenhancedvideoplayer/components/playerOverlay/SeekHandler.kt
@@ -0,0 +1,157 @@
+package com.profusion.androidenhancedvideoplayer.components.playerOverlay
+
+import androidx.compose.animation.core.FastOutSlowInEasing
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.media3.exoplayer.ExoPlayer
+import com.profusion.androidenhancedvideoplayer.styling.Colors
+import com.profusion.androidenhancedvideoplayer.utils.JobsHolder
+import com.profusion.androidenhancedvideoplayer.utils.executeAfterTimeout
+import kotlin.math.max
+import kotlin.math.min
+
+private const val ICON_ANIMATION_DURATION_MS = 650
+private const val ICON_INITIAL_SCALE = 0.8f
+private const val ICON_TARGET_SCALE = 1.1f
+private const val JOB_TIMEOUT = 650L
+private const val TRANSITION_LABEL = "scaleSeekIcon"
+
+@Composable
+fun SeekHandler(
+    disableSeekForward: Boolean,
+    isControlsVisible: Boolean,
+    exoPlayer: ExoPlayer,
+    toggleControlsVisibility: () -> Unit,
+    setControlsVisibility: (value: Boolean) -> Unit,
+    transformSeekIncrementRatio: (tapCount: Int) -> Long,
+    controlsCustomization: ControlsCustomization
+) {
+    val jobs = JobsHolder
+    val scope = rememberCoroutineScope()
+    var forwardTapCount by remember { mutableStateOf(0) }
+    var rewindTapCount by remember { mutableStateOf(0) }
+    val isRewinding by remember { derivedStateOf { rewindTapCount > 0 } }
+    val isForwarding by remember { derivedStateOf { forwardTapCount > 0 } }
+
+    val transition = rememberInfiniteTransition(TRANSITION_LABEL)
+
+    val scale = transition.animateFloat(
+        initialValue = ICON_INITIAL_SCALE,
+        targetValue = ICON_TARGET_SCALE,
+        animationSpec = infiniteRepeatable(
+            animation = tween(
+                durationMillis = ICON_ANIMATION_DURATION_MS,
+                easing = FastOutSlowInEasing
+            ),
+            repeatMode = RepeatMode.Restart
+        )
+    )
+
+    LaunchedEffect(forwardTapCount) {
+        if (forwardTapCount > 0) {
+            val incrementTime = transformSeekIncrementRatio(forwardTapCount) -
+                transformSeekIncrementRatio(
+                    forwardTapCount - 1
+                )
+            val timeToSeek = exoPlayer.currentPosition + incrementTime
+            exoPlayer.seekTo(min(exoPlayer.duration, timeToSeek))
+        }
+    }
+
+    LaunchedEffect(rewindTapCount) {
+        if (rewindTapCount > 0) {
+            val incrementTime = transformSeekIncrementRatio(rewindTapCount) -
+                transformSeekIncrementRatio(
+                    rewindTapCount - 1
+                )
+            val timeToSeek = exoPlayer.currentPosition - incrementTime
+            exoPlayer.seekTo(max(0, timeToSeek))
+        }
+    }
+
+    fun checkIfCanToggleIsControlsVisible() {
+        if (!isRewinding && !isForwarding) {
+            toggleControlsVisibility()
+        }
+    }
+
+    fun onForwardSingleTap() {
+        jobs.seekJob = executeAfterTimeout(scope, jobs.seekJob, JOB_TIMEOUT) {
+            forwardTapCount = 0
+        }
+        forwardTapCount++
+    }
+
+    fun onForwardDoubleTap() {
+        if (isRewinding) return
+        setControlsVisibility(false)
+        onForwardSingleTap()
+    }
+
+    fun onRewindSingleTap() {
+        jobs.seekJob = executeAfterTimeout(scope, jobs.seekJob, JOB_TIMEOUT) {
+            rewindTapCount = 0
+        }
+        rewindTapCount++
+    }
+
+    fun onRewindDoubleTap() {
+        if (isForwarding) return
+        setControlsVisibility(false)
+        onRewindSingleTap()
+    }
+
+    Row(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(
+                if (isRewinding || isForwarding) {
+                    Colors.controlsShadow
+                } else {
+                    Color.Transparent
+                }
+            )
+            .clickable { setControlsVisibility(!isControlsVisible) }
+    ) {
+        SeekClickableArea(
+            modifier = Modifier.weight(1f),
+            tapCount = rewindTapCount,
+            scaleAnimation = scale.value,
+            onSeekDoubleTap = ::onRewindDoubleTap,
+            onSeekSingleTap = ::onRewindSingleTap,
+            checkIfCanToggleIsControlsVisible = ::checkIfCanToggleIsControlsVisible,
+            getSeekTime = { (transformSeekIncrementRatio(rewindTapCount) / 1000).toInt() },
+            seekIcon = { controlsCustomization.rewindIconContent(it) }
+        )
+        Spacer(modifier = Modifier.weight(0.2f))
+        SeekClickableArea(
+            modifier = Modifier.weight(1f),
+            tapCount = forwardTapCount,
+            scaleAnimation = scale.value,
+            disableSeekClick = disableSeekForward,
+            onSeekDoubleTap = ::onForwardDoubleTap,
+            onSeekSingleTap = ::onForwardSingleTap,
+            checkIfCanToggleIsControlsVisible = ::checkIfCanToggleIsControlsVisible,
+            getSeekTime = { (transformSeekIncrementRatio(forwardTapCount) / 1000).toInt() },
+            seekIcon = { controlsCustomization.forwardIconContent(it) }
+        )
+    }
+}
