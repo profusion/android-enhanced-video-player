695bb4a1639f3eb44dcc88a4b86769378460284a
Thalys Matias Carrara <thalys.carrara@profusion.mobi>
2023-06-07 22:24:08 -0400

    feat: implement forward/rewind tests
    Closes #15

--- androidenhancedvideoplayer/src/androidTest/java/com/profusion/androidenhancedvideoplayer/test/EnhancedVideoPlayerTest.kt
+++ androidenhancedvideoplayer/src/androidTest/java/com/profusion/androidenhancedvideoplayer/test/EnhancedVideoPlayerTest.kt
@@ -1,9 +1,12 @@
 package com.profusion.androidenhancedvideoplayer.test
 
 import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.doubleClick
 import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onAllNodesWithTag
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.performClick
+import androidx.compose.ui.test.performTouchInput
 import com.profusion.androidenhancedvideoplayer.components.EnhancedVideoPlayer
 import org.junit.Rule
 import org.junit.Test
@@ -27,6 +30,40 @@ class EnhancedVideoPlayerTest {
             .performClick()
 
         composeTestRule.onNodeWithTag("PlayerControlsParent", useUnmergedTree = true)
+            .assertIsDisplayed()
+    }
+
+    @Test
+    fun enhancedVideoPlayer_WhenDoubleClickHappenOnTheFirstHalfOfScreenVideoShouldShowRewindIcon() {
+        composeTestRule.setContent {
+            EnhancedVideoPlayer(
+                resourceId = R.raw.login_screen_background
+            )
+        }
+
+        composeTestRule.onAllNodesWithTag("SeekClickableArea", useUnmergedTree = true)[0]
+            .performTouchInput {
+                doubleClick()
+            }
+
+        composeTestRule.onNodeWithTag("RewindIcon", useUnmergedTree = true)
+            .assertIsDisplayed()
+    }
+
+    @Test
+    fun enhancedVideoPlayer_WhenDoubleClickHappenOnTheLastHalfOfScreenVideoShouldShowRewindIcon() {
+        composeTestRule.setContent {
+            EnhancedVideoPlayer(
+                resourceId = R.raw.login_screen_background
+            )
+        }
+
+        composeTestRule.onAllNodesWithTag("SeekClickableArea", useUnmergedTree = true)[1]
+            .performTouchInput {
+                doubleClick()
+            }
+
+        composeTestRule.onNodeWithTag("ForwardIcon", useUnmergedTree = true)
             .assertIsDisplayed()
     }
 }
