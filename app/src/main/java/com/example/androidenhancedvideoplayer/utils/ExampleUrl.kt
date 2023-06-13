17bf9c4166c602d27837b1ee6793c1cadbf4c31f
Andrey Freitas <yanusckiewicz@profusion.mobi>
2023-06-13 18:10:28 -0300

    refactor(URLs): add all example urls to a single file
    closes #59

---
+++ app/src/main/java/com/example/androidenhancedvideoplayer/utils/ExampleUrl.kt
@@ -0,0 +1,10 @@
+package com.example.androidenhancedvideoplayer.utils
+
+object ExampleUrl {
+    const val MSS = "https://playready.directtaps.net/" +
+        "smoothstreaming/SSWSS720H264/SuperSpeedway_720.ism/Manifest"
+    const val DASH = "https://dash.akamaized.net/akamai/bbb_30fps/bbb_30fps.mpd"
+    const val HLS = "https://demo-streaming.gcdn.co/videos/676_YJHUNNocCsrjiCDx/master.m3u8"
+    const val MP4 = "https://commondatastorage.googleapis.com/" +
+        "gtv-videos-bucket/sample/ElephantsDream.mp4"
+}
