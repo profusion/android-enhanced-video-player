9d5bcc694d521394c3a367f1057a148bacf8b278
Thalys Matias Carrara <thalys.carrara@profusion.mobi>
2023-06-15 16:42:26 -0400

    feat: implement functions to change activity brightness
    Closes #49

--- androidenhancedvideoplayer/src/main/java/com/profusion/androidenhancedvideoplayer/utils/Extensions.kt
+++ androidenhancedvideoplayer/src/main/java/com/profusion/androidenhancedvideoplayer/utils/Extensions.kt
@@ -5,11 +5,15 @@ import android.content.Context
 import android.content.ContextWrapper
 import android.content.pm.ActivityInfo
 import android.content.res.Configuration
+import android.view.WindowManager
+import android.view.WindowManager.LayoutParams.BRIGHTNESS_OVERRIDE_NONE
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.ui.Modifier
 import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.WindowInsetsControllerCompat
+
+private const val MED_BRIGHTNESS = 0.5f
 
 fun Context.findActivity(): Activity? = when (this) {
     is Activity -> this
@@ -57,4 +61,30 @@ fun Context.setNavigationBarVisibility(showNavigationBar: Boolean) {
             windowInsetsController.hide(WindowInsetsCompat.Type.navigationBars())
         }
     }
+}
+fun Context.setActivityBrightness(brightness: Float) {
+    val window = this.findActivity()?.window
+
+    window?.let {
+        val layout: WindowManager.LayoutParams? = it.attributes
+        layout?.screenBrightness = brightness.coerceIn(0f, 1f)
+        it.attributes = layout
+    }
+}
+
+fun Context.resetActivityBrightnessToDefault() {
+    val window = this.findActivity()?.window
+
+    window?.let {
+        val layout: WindowManager.LayoutParams? = it.attributes
+        layout?.screenBrightness = BRIGHTNESS_OVERRIDE_NONE
+        layout?.buttonBrightness = BRIGHTNESS_OVERRIDE_NONE
+        it.attributes = layout
+    }
+}
+
+fun Context.getActivityBrightnessOrDefault(default: Float = MED_BRIGHTNESS): Float {
+    val window = this.findActivity()?.window
+    val brightness = window?.attributes?.screenBrightness ?: default
+    return if (brightness < 0) default else brightness
 }
