0732f708274c0a74e332ef03f03cea95b27ae548
Andrey Freitas <yanusckiewicz@profusion.mobi>
2023-06-23 09:42:21 -0300

    feat(quality): add track quality selection logic
    - add TrackQualityItem to be used in quality selection
    - add TrackUtils to hold track selection related functions
    
    closes #50

---
+++ androidenhancedvideoplayer/src/main/java/com/profusion/androidenhancedvideoplayer/utils/TrackUtils.kt
@@ -0,0 +1,89 @@
+package com.profusion.androidenhancedvideoplayer.utils
+
+import androidx.annotation.OptIn
+import androidx.media3.common.C
+import androidx.media3.common.Format
+import androidx.media3.common.TrackSelectionOverride
+import androidx.media3.common.TrackSelectionParameters
+import androidx.media3.common.Tracks
+import androidx.media3.common.util.UnstableApi
+import androidx.media3.exoplayer.ExoPlayer
+
+private const val NO_ROLE_FLAGS = 0
+
+@OptIn(UnstableApi::class)
+fun generateTrackQualityOptions(tracks: Tracks): List<TrackQualityItem> {
+    val qualityOptions = ArrayList<TrackQualityItemValue>()
+
+    for (group in tracks.groups) {
+        for (trackIndex in 0 until group.length) {
+            if (group.isTrackSupported(trackIndex) &&
+                group.mediaTrackGroup.type == C.TRACK_TYPE_VIDEO &&
+                group.getTrackFormat(trackIndex).isVideoTrackPlayable()
+            ) {
+                qualityOptions += TrackQualityItemValue(
+                    group = group.mediaTrackGroup,
+                    trackIndex = trackIndex
+                )
+            }
+        }
+    }
+
+    return qualityOptions.sortedDescending()
+}
+
+@UnstableApi
+fun TrackSelectionParameters.getChangedTrackSelection(
+    trackType: Int
+): TrackSelectionOverride? = overrides.values.firstOrNull { it.type == trackType }
+
+@UnstableApi
+fun TrackSelectionParameters.getSelectedTrackQualityItem(
+    autoLabel: String
+): TrackQualityItem {
+    val videoTrackOverride = getChangedTrackSelection(C.TRACK_TYPE_VIDEO)
+        ?: return TrackQualityAuto(autoLabel)
+
+    val index = videoTrackOverride.trackIndices.single()
+        ?: return TrackQualityAuto(autoLabel)
+
+    val group = videoTrackOverride.mediaTrackGroup
+
+    return TrackQualityItemValue(
+        group = group,
+        trackIndex = index
+    )
+}
+
+fun ExoPlayer.setVideoQuality(track: TrackQualityItem) {
+    trackSelectionParameters = when (track) {
+        is TrackQualityAuto -> {
+            trackSelectionParameters
+                .buildUpon()
+                .clearOverridesOfType(C.TRACK_TYPE_VIDEO)
+                .build()
+        }
+        is TrackQualityItemValue -> {
+            trackSelectionParameters
+                .buildUpon()
+                .clearOverridesOfType(C.TRACK_TYPE_VIDEO)
+                .addOverride(
+                    TrackSelectionOverride(
+                        track.group,
+                        track.trackIndex
+                    )
+                )
+                .build()
+        }
+    }
+}
+
+@OptIn(UnstableApi::class)
+fun Format.buildQualityLabel(): String {
+    return "${height}p - ${bitrate / 1000}kbps"
+}
+
+@OptIn(UnstableApi::class)
+private fun Format.isVideoTrackPlayable(): Boolean {
+    return roleFlags == NO_ROLE_FLAGS
+}
