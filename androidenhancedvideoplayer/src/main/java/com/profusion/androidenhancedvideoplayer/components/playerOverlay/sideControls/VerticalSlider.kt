8e06e8b59b2ddc256ab0f5257cf50b49e7db59b6
Thalys Matias Carrara <thalys.carrara@profusion.mobi>
2023-06-29 18:37:25 -0400

    refactor: remove local state from VerticalSlider
    Closes #52

--- androidenhancedvideoplayer/src/main/java/com/profusion/androidenhancedvideoplayer/components/playerOverlay/sideControls/VerticalSlider.kt
+++ androidenhancedvideoplayer/src/main/java/com/profusion/androidenhancedvideoplayer/components/playerOverlay/sideControls/VerticalSlider.kt
@@ -1,38 +1,32 @@
-package com.profusion.androidenhancedvideoplayer.components.playerOverlay
+package com.profusion.androidenhancedvideoplayer.components.playerOverlay.sideControls
 
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Slider
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.unit.dp
 
+val DEFAULT_SLIDER_RANGE = 0f..1f
 private val DEFAULT_HEIGHT = 100.dp
 private const val ROTATION_DEGREES = 270f
 
 @Composable
 fun VerticalSlider(
     modifier: Modifier = Modifier,
-    initialValue: Float,
+    value: Float,
+    valueRange: ClosedFloatingPointRange<Float> = DEFAULT_SLIDER_RANGE,
     interactionSource: MutableInteractionSource,
     onValueChange: (value: Float) -> Unit,
     onValueChangeFinished: (() -> Unit)? = null
 ) {
-    var value by remember { mutableStateOf(initialValue) }
-
     Slider(
         modifier = modifier
             .rotate(ROTATION_DEGREES).width(DEFAULT_HEIGHT),
         value = value,
-        onValueChange = {
-            value = it
-            onValueChange(it)
-        },
+        valueRange = valueRange,
+        onValueChange = onValueChange,
         onValueChangeFinished = onValueChangeFinished,
         interactionSource = interactionSource
     )
